9a3070b9b5a514bee193b885f2f7e946a74d8af7
Jaan Van Gils <j.j.g.vangils@students.uu.nl>
2022-01-09 17:53:49 +0100

    Preprocessing: add labels for app name and type and select features from data
    Co-authored-by: Maarten <maarten6@users.noreply.github.com>

--- preprocess.py
+++ preprocess.py
@@ -2,19 +2,35 @@ import pandas as pd
 import os
 import re
 
-BASE_DIR = "data/CSV/VPN-PCAPs-02/"
+BASE_DIR = "data/CSV/"
+BASE_DIR_PROCESSED = "data/CSV-Labelled/"
+CSV_FEATURES = ["Flow ID","Src IP","Src Port","Dst IP","Dst Port","Protocol"]
 
-file_names = [file for file in os.listdir(BASE_DIR)]
-frames = []
+# File name format:
+# applicationType_applicationName_index[a | b].pcap_Flow.csv
 
-regex = re.compile("^(.*)_\d*.*\.pcap_Flow.*")
+def process_files():
+    """Goes through all files and folders in 'BASE_DIR' and adds labels for the application type and application name to them.
+    Will also remove the features that are not required by the model.
 
-for name in file_names:
-    match = regex.search(name)
-    pcap_type = match.group(1)
-    df = pd.read_csv(BASE_DIR + name)
-    df["label"] = pcap_type
-    frames.append(df)
+    Make sure that all files conform to the format "applicationType_applicationName_index[a | b].pcap_Flow.csv", where [a | b] means an optional character that is 'a' or 'b'.
+    """
 
-csv = pd.concat(frames)
-csv.to_csv("compleet.csv", index=False)
+    regex = re.compile("^([^_]*)_(.*)_\d*(.*)\.pcap_Flow.csv")
+
+    for root, dirs, files in os.walk(BASE_DIR):
+        for name in files:
+            if name.endswith((".pcap_Flow.csv")):
+                filename = os.path.join(root, name)
+                match = regex.search(name)
+                application_type = match.group(1)
+                application_name = match.group(2)
+                df = pd.read_csv(filename)[CSV_FEATURES]
+                df["Label"] = application_type
+                df["Application"] = application_name
+
+                if not os.path.exists(BASE_DIR_PROCESSED + root):
+                    os.makedirs(BASE_DIR_PROCESSED + root)
+                df.to_csv(BASE_DIR_PROCESSED + filename, index=False)
+
+process_files()
