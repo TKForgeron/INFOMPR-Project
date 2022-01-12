e4a36f6752f5b8617673833d3afa0375970e75b9
Jaan Van Gils <j.j.g.vangils@students.uu.nl>
2022-01-12 12:20:21 +0100

    Upgrade CNN and remove randomness
    Co-authored-by: Maarten <maarten6@users.noreply.github.com>

--- cnn.py
+++ cnn.py
@@ -3,31 +3,63 @@ import tensorflow as tf
 from tensorflow import keras
 import preprocess as pp
 from nn_layers import *
+import random as python_random
+import os
 
 NUM_FEATURES = pp.total_features()  # amount of fields in the input
-NUM_LABELS = pp.total_labels()  # amount of tags in the output
+LABELS = pp.labels()  # amount of tags in the output
+
+
+def prod(tuple):
+    """Returns the product of all elements in the given tuple."""
+    r = 1
+    for i in list(tuple):
+        if i is not None:
+            r *= i
+    return r
+
+
+# BASIC SETUP
+
+tf.random.set_seed(42)
+np.random.seed(42)
+python_random.seed(42)
+os.environ["PYTHONHASHSEED"] = "0"
 
 
 # MODEL SETUP
 
 input_layer = keras.Input(shape=(NUM_FEATURES))
-reshape = create_reshape_layer((NUM_FEATURES,), (NUM_FEATURES, 1))(input_layer)
+reshape = create_reshape_layer((NUM_FEATURES,), (1, NUM_FEATURES, 1))(
+    input_layer
+)  # Needs to be reshapen to the input shape of the conv layer
 
-conv = create_convolutional_layer(NUM_FEATURES, int(NUM_FEATURES / 2 + 1))(reshape)
-pool = create_max_pool_layer()(conv)
-# conv = create_convolutional_layer(NUM_FEATURES, int(NUM_FEATURES / 4) + 1)(pool)
-# pool = create_max_pool_layer()(conv)
-dense = create_dense_layer(NUM_LABELS)(pool)
-reshape = create_reshape_layer((2, NUM_LABELS), (NUM_LABELS * 2,))(dense)
-dense = create_dense_layer(NUM_LABELS)(reshape)
-dense = create_dense_layer(NUM_LABELS)(dense)
+# TODO: tune the parameters of all layers below
+conv = create_convolutional_layer(32, kernel_size=(1, 2))(
+    reshape
+)  # data_format = channels_last corresponds to inputs with shape (batch_size, height, width, channels) while channels_first corresponds to inputs with shape (batch_size, channels, height, width)
+pool = create_max_pool_layer(pool_size=(1, 2))(conv)
+bn = create_batch_normalisation_layer()(pool)
+conv = create_convolutional_layer(64, kernel_size=(1, 2))(reshape)
+pool = create_max_pool_layer(pool_size=(1, 2))(conv)
+bn = create_batch_normalisation_layer()(pool)
+reshape = create_reshape_layer(pool.shape, (prod(pool.shape),))(bn)
+dense = create_dense_layer(200)(reshape)
 
-softmax = create_softmax_layer()(input_layer)
-model = keras.Model(inputs=input_layer, outputs=softmax)
-model.summary()
+outputs = []
+for label in LABELS:
+    name_output, num_outputs = label
+    dense2 = create_dense_layer(num_outputs, name="dense_" + str(name_output))(dense)
+    softmax = create_softmax_layer(name="softmax_" + str(name_output))(dense2)
+    outputs.append(softmax)
+
+model = keras.Model(inputs=input_layer, outputs=outputs)
+# model.summary()
 # keras.utils.plot_model(model, "GRU_model.png", show_shapes=True)
 
+
 # MODEL TRAINING
+
 x_train, x_val, x_test, t_train, t_val, t_test = pp.get_train_validation_test_set()
 
 # TODO: set compiler variables
@@ -38,9 +70,18 @@ model.compile(
 )
 
 # TODO: tune parameters
-model.fit(x_train, t_train, batch_size=64, epochs=20, validation_data=(x_val, t_val))
+model.fit(
+    x_train,
+    t_train,
+    batch_size=64,
+    epochs=20,
+    validation_data=(x_val, t_val),
+    verbose=0,
+)
 
 # MODEL TESTING
-test_scores = model.evaluate(x_test, t_test, verbose=2)
-print("Test loss:", test_scores[0])
-print("Test accuracy:", test_scores[1])
+
+test_metric_names = model.metrics_names
+test_scores = model.evaluate(x_test, t_test, verbose=0)
+for idx, score in enumerate(test_scores):
+    print(test_metric_names[idx], ": ", score)
--- nn_layers.py
+++ nn_layers.py
@@ -3,11 +3,12 @@ from tensorflow.keras import layers
 ## COMMON LAYERS
 
 
-def create_dense_layer(units):
+def create_dense_layer(units, name=None):
     """Create a new fully connected layer
 
     Keyword arguments:
     units -- Positive integer, dimensionality of the output space.
+    [name] -- String name of the layer.
     Note: more can be added if deemed necessary later on
     """
     return layers.Dense(
@@ -21,6 +22,7 @@ def create_dense_layer(units):
         activity_regularizer=None,
         kernel_constraint=None,
         bias_constraint=None,
+        name=name,
     )
 
 
@@ -45,9 +47,9 @@ def create_convolutional_layer(filters, kernel_size):
 
     Keyword arguments:
     filters -- Integer, the dimensionality of the output space (i.e. the number of output filters in the convolution).
-    kernel_size -- An integer or tuple/list of a single integer, specifying the length of the 1D convolution window.
+    kernel_size -- An integer or tuple/list of 2 integers, specifying the height and width of the 2D convolution window. Can be a single integer to specify the same value for all spatial dimensions.
     """
-    return layers.Conv1D(
+    return layers.Conv2D(
         filters,
         kernel_size,
         strides=1,
@@ -67,17 +69,44 @@ def create_convolutional_layer(filters, kernel_size):
     )
 
 
-def create_max_pool_layer():
-    """Create a new max pooling  (i.e. value of pooled cell will be the maximum of all cells in the window)"""
-    return layers.MaxPooling1D(
-        pool_size=2, strides=None, padding="valid", data_format="channels_last"
+def create_batch_normalisation_layer():
+    """Create a new batch normalisation layer"""
+    return layers.BatchNormalization(
+        axis=-1,
+        momentum=0.99,
+        epsilon=0.001,
+        center=True,
+        scale=True,
+        beta_initializer="zeros",
+        gamma_initializer="ones",
+        moving_mean_initializer="zeros",
+        moving_variance_initializer="ones",
+        beta_regularizer=None,
+        gamma_regularizer=None,
+        beta_constraint=None,
+        gamma_constraint=None,
     )
 
 
-def create_avg_pool_layer():
-    """Create a new average pooling (i.e. value of pooled cell will be the average of all cells in the window)"""
-    return layers.AveragePooling1D(
-        pool_size=2, strides=None, padding="valid", data_format="channels_last"
+def create_max_pool_layer(pool_size=2):
+    """Create a new max pooling  (i.e. value of pooled cell will be the maximum of all cells in the window)
+
+    Keyword arguments:
+    [pool_size] -- integer or tuple of 2 integers, window size over which to take the maximum. (2, 2) will take the max value over a 2x2 pooling window. If only one integer is specified, the same window length will be used for both dimensions. Default is (2,2).
+    """
+    return layers.MaxPooling2D(
+        pool_size=pool_size, strides=None, padding="valid", data_format="channels_last"
+    )
+
+
+def create_avg_pool_layer(pool_size=2):
+    """Create a new average pooling (i.e. value of pooled cell will be the average of all cells in the window)
+
+    Keyword arguments:
+    [pool_size] -- integer or tuple of 2 integers, window size over which to take the maximum. (2, 2) will take the max value over a 2x2 pooling window. If only one integer is specified, the same window length will be used for both dimensions. Default is (2,2).
+    """
+    return layers.AveragePooling2D(
+        pool_size=pool_size, strides=None, padding="valid", data_format="channels_last"
     )
 
 
